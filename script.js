c9984303d2d2427a117b060e4b777fd1c3f44b65
Nathan Smith <nsmith10@sheffield.ac.uk>
2026-02-03 20:15:17 +0000

    new card

--- script.js
+++ script.js
@@ -36,30 +36,21 @@ noBtn.addEventListener("mouseover", () => {
     noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
 });
 
-// Logic to make YES btn to grow
-
-// let yesScale = 1;
-
-// yesBtn.style.position = "relative"
-// yesBtn.style.transformOrigin = "center center";
-// yesBtn.style.transition = "transform 0.3s ease";
-
-// noBtn.addEventListener("click", () => {
-//     yesScale += 2;
-
-//     if (yesBtn.style.position !== "fixed") {
-//         yesBtn.style.position = "fixed";
-//         yesBtn.style.top = "50%";
-//         yesBtn.style.left = "50%";
-//         yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
-//     }else{
-//         yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
-//     }
-// });
-
-// YES is clicked
 
 yesBtn.addEventListener("click", () => {
+    title.textContent = "Good Girl 😛";
+
+    catImg.src = "win-cat.jpg";
+
+    document.querySelector(".letter-window").classList.add("final");
+
+    buttons.style.display = "none";
+
+    finalText.style.display = "block";
+});
+
+
+continueBtn.addEventListener("click", () => {
     title.textContent = "Yippeeee!";
 
     catImg.src = "cat_dance.gif";
@@ -70,3 +61,4 @@ yesBtn.addEventListener("click", () => {
 
     finalText.style.display = "block";
 });
+
